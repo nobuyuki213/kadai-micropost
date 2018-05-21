e4858594c4f180b5e27b49acebbe4db7b81fbbb3
EC2 Default User <ec2-user@ip-172-31-42-43.us-east-2.compute.internal>
2018-05-21 09:33:40 +0000

    top page, init laravelcollective

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Microposts</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv-"X-UA-comoatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initinal-scale-1">
+        <title>@yield('title')-Microposts</title>
+        
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <style>
+            body {
+                background: url(http://www.tokkoro.com/picsup/2939281-text-writing-minimalism-typography-digital-art___abstract-wallpapers.jpg);
+                background-position: top center;
+                background-size: cover;
+                height: 900px;
+            }
+        </style>
+        
+    </head>
+    <body>
+        @include('commons.navbar')
+        
+        <div class="container">
+            @include('commons.error_messages')
+            
+            @yield('content')
+        </div>
+    </body>
+</html>
