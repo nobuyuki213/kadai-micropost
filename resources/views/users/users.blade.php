5aac6ea90546baf49e40ac93875b5f62f56d17fb
EC2 Default User <ec2-user@ip-172-31-42-43.us-east-2.compute.internal>
2018-05-23 05:48:31 +0000

    user pages

---
+++ resources/views/users/users.blade.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <div class="col-sm-6 col-sm-offset-3">
+        @if(count($users) > 0)
+        <ul class="media-list">
+            @foreach($users as $user)
+                <li class="media">
+                    <div class="media-left">
+                        <a href="#">
+                            <img class="media-object img-thumbnail img-responsive" src="{{ Gravatar::src($user->email, 50) }}" alt="">
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        <div class="">
+                            <h4 class="media-heading">{{ $user->name }}</h4>
+                        </div>
+                        <div class="">
+                            {!! link_to_route('users.show', 'View Profile', ['id' => $user->id], ['class' => 'btn btn-primary btn-xs']) !!}
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+        <div class="text-center">
+            {!! $users->render() !!}
+        </div>
+        @endif
+    </div>
+</div>
